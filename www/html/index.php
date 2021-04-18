9c1dfa82a6ebc228242f8471c6589af58ae614dc
ota <chikayo.ot.2@gmail.com>
2021-04-18 23:03:20 +0900

    商品並べ替え機能を実装しました。#16

--- www/html/index.php
+++ www/html/index.php
@@ -12,9 +12,16 @@ if(is_logined() === false){
 
 $token = get_csrf_token();
 
+// 並べ替え情報取得
+$order = $_GET['order'];
+
+
+// DB接続
 $db = get_db_connect();
 $user = get_login_user($db);
 
-$items = get_open_items($db);
+
+// 公開商品の取得
+$items = get_open_items($db,$order);
 
 include_once VIEW_PATH . 'index_view.php';
